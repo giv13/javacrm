948f1cec1062c0efdd1c64940daf71aa25a749d4
Ilia Grigorev <giv13@bk.ru>
2025-02-10 20:29:47 +0500

    Перенос в подпапку frontend; установка Spring Boot в backend

---
+++ backend/src/test/java/ru/giv13/infocrm/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.giv13.infocrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
