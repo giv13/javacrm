39869caac5aae6840d0c725294cb5a38dc4e89f2
Ilia Grigorev <giv13@bk.ru>
2025-05-14 17:21:18 +0500

    Настройка Docker

--- backend/src/main/java/ru/giv13/javacrm/system/AppConfig.java
+++ backend/src/main/java/ru/giv13/javacrm/system/AppConfig.java
@@ -1,6 +1,7 @@
 package ru.giv13.javacrm.system;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
@@ -9,12 +10,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig {
+    @Value("${cors.origin}")
+    private String corsOrigin;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:5173").allowedMethods("*").allowCredentials(true);
+                registry.addMapping("/**").allowedOrigins(corsOrigin).allowedMethods("*").allowCredentials(true);
             }
         };
     }
